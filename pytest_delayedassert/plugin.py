acd65b04eda137b4950815bdc872226009d371eb
Ashley C Straw <as.fireflash38@gmail.com>
2015-08-28 11:16:26 -0400

    Modified plugin to no longer use fixtures
    Plugin now hooks directly into the pytest module, so you can do:
    
    pytest.assume(1 == 1, "unequal")
    
    Instead of requiring a fixture for each test that has an assumption

--- pytest_delayedassert/plugin.py
+++ pytest_delayedassert/plugin.py
@@ -2,36 +2,36 @@ import pytest
 import inspect
 import os.path
 
-@pytest.fixture
-def delayed_assert(request):
-    def do_expect(expr, msg=''):
+def pytest_namespace():
+    def assume(expr, msg=''):
         if not expr:
-            _log_failure(request.node, msg)
-    return do_expect
+            # get filename, line, and context
+            (filename, line, funcname, contextlist) = inspect.stack()[1][1:5]
+            filename = os.path.basename(filename)
+            context = contextlist[0]
+            # format entry
+            msg = '%s\n' % msg if msg else ''
+            entry = '>%s%s%s:%s\n--------' % (context, msg, filename, line)
+            # add entry
+            pytest._failed_assumptions.append(entry)
 
-def _log_failure(node, msg=''):
-    # get filename, line, and context
-    (filename, line, funcname, contextlist) =  inspect.stack()[2][1:5]
-    filename = os.path.basename(filename)
-    context = contextlist[0]
-    # format entry
-    msg = '%s\n' % msg if msg else ''
-    entry = '>%s%s%s:%s\n--------' % (context, msg, filename, line)
-    # add entry 
-    if not hasattr(node, '_failed_expect'):
-        node._failed_expect = []
-    node._failed_expect.append(entry)
+    return {'_failed_assumptions': [],
+            'assume': assume}
+
+def pytest_runtest_setup(item):
+    del pytest._failed_assumptions[:]
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
-    if (call.when == "call") and hasattr(item, '_failed_expect'):
-        summary = 'Failed Expectations:%s' % len(item._failed_expect)
-        item._failed_expect.append(summary)
+    if call.when == "call" and pytest._failed_assumptions:
+        summary = 'Failed Assumptions:%s' % len(pytest._failed_assumptions)
+        pytest._failed_assumptions.append(summary)
         if report.longrepr:
-            report.longrepr = str(report.longrepr) + '\n--------\n' + ('\n'.join(item._failed_expect))
+            report.longrepr = str(report.longrepr) + \
+                              '\n--------\n' + ('\n'.join(pytest._failed_assumptions))
         else:
-            report.longrepr = '\n'.join(item._failed_expect)
+            report.longrepr = '\n'.join(pytest._failed_assumptions)
         report.outcome = "failed"
 
