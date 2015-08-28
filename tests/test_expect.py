7f3e20042d82b497c1fc7bfb059136351ddff6a1
Ashley C Straw <as.fireflash38@gmail.com>
2015-08-28 11:22:41 -0400

    Updated tests

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -4,8 +4,10 @@ pytest_plugins = "pytester",
 def test_passing_expect(testdir):
     testdir.makepyfile(
         """
-        def test_func(expect):
-            expect(1 == 1)
+        import pytest
+        
+        def test_func():
+            pytest.assume(1 == 1)
         """)
     result = testdir.runpytest()
     assert '1 passed' in result.stdout.str()
@@ -13,8 +15,9 @@ def test_passing_expect(testdir):
 def test_failing_expect(testdir):
     testdir.makepyfile(
         """
-        def test_func(expect):
-            expect(1 == 2)
+        import pytest
+        def test_func():
+            pytest.assume(1 == 2)
         """)
     result = testdir.runpytest()
     assert '1 failed' in result.stdout.str()
@@ -23,8 +26,9 @@ def test_failing_expect(testdir):
 def test_passing_expect_doesnt_cloak_assert(testdir):
     testdir.makepyfile(
         """
-        def test_func(expect):
-            expect(1 == 1)
+        import pytest
+        def test_func():
+            pytest.assume(1 == 1)
             assert 1 == 2
         """)
     result = testdir.runpytest()
@@ -34,8 +38,9 @@ def test_passing_expect_doesnt_cloak_assert(testdir):
 def test_failing_expect_doesnt_cloak_assert(testdir):
     testdir.makepyfile(
         """
-        def test_func(expect):
-            expect(1 == 2)
+        import pytest
+        def test_func():
+            pytest.assume(1 == 2)
             assert 1 == 2
         """)
     result = testdir.runpytest()
@@ -46,10 +51,11 @@ def test_failing_expect_doesnt_cloak_assert(testdir):
 def test_msg_is_in_output(testdir):
     testdir.makepyfile(
         """
-        def test_func(expect):
+        import pytest
+        def test_func():
             a = 1
             b = 2
-            expect(a == b, 'a:%s b:%s' % (a,b))
+            pytest.assume(a == b, 'a:%s b:%s' % (a,b))
         """)
     result = testdir.runpytest()
     assert '1 failed' in result.stdout.str()
